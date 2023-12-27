df0d724d3e17605c1da3f40e61e12b9ad01e84d6
Akunji <149184042+Akunji@users.noreply.github.com>
2023-12-27 20:09:36 +0100

    Initial commit

---
+++ dav.js
@@ -0,0 +1,33 @@
+
+
+displayTime = () =>{
+    let date = new Date();
+    let hour = date.getHours;
+    let min = date.getMinutes;
+    let eba = hour.toString()
+console.log(eba)
+
+}
+
+displayTime()
+
+
+
+
+/*
+ <div class="second-side comments-container">
+        <h1>People also say:</h1>
+<div class="comments-div">
+    <div class="commenters-name">
+        <h3>Mr Fransis Jhonson: <span>CEO Asho & co</span></h3>
+    </div>
+    <div class="comment">
+        <p>
+            i strongly appreciate this fellow for he is hightly competent 
+            and useull in life. ill rate him a 8/10
+        </p>
+    </div>
+</div>
+    </div>
+</div><!--second-container-->
+*/
